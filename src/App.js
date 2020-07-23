f1c348d7bbe4eb3e563fbc30b32816f0a52a42d8
Christopher W Schultz <chris.william.schultz@gmail.com>
2020-07-22 20:25:52 -0600

    added browserrouter and router, imported missing components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React, { useState } from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import { mockUser } from './dummyData'
 
 import UserContext from "./contexts/userContext";
+
+import Navigation from "./components/core/navigation";
+import PrivateRoute from "./components/routes/privateRoute";
+import PlantPage from "./components/plants/plantPage";
+import MarketingPage from "./components/marketing/marketingPage";
 
 
 function App() {
     const [userInfo, setUserInfo] = useState({});
 
   return (
-      <div>
-          <UserContext.Provider value={userInfo}>
-              <Navigation/>
-          </UserContext.Provider>
+      <Router>
+          <div>
+              <UserContext.Provider value={userInfo}>
+                  <Navigation/>
+              </UserContext.Provider>
 
-          {localStorage.getItem('token') ? <PrivateRoute component={PlantPage}/> : <Route exact path="/" component={MarketingPage}/>}
-      </div>
+              {localStorage.getItem('token') ? <PrivateRoute component={PlantPage}/> : <Route exact path="/" component={MarketingPage}/>}
+          </div>
+      </Router>
   );
 }
 
