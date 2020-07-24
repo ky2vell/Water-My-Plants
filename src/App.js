87705e0d928f2cac9d2c91dd6755b460bf722970
Christopher W Schultz <chris.william.schultz@gmail.com>
2020-07-23 20:16:27 -0600

    login and inital fetching of data Co-authored-by: Kyle Lovell <ky2vell@gmail.com> Co-authored-by: Chris Schultz <chris.william.schultz@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
-import { mockUser } from './dummyData'
-
-import UserContext from "./contexts/userContext";
 
 import Navigation from "./components/core/navigation";
 import PrivateRoute from "./components/routes/privateRoute";
-import PlantPage from "./components/plants/plantPage";
 import MarketingPage from "./components/marketing/marketingPage";
+
+import PlantContext from "./contexts/plantsContext";
+import PlantList from "./components/plants/plantList";
 
 
 function App() {
-    const [userInfo, setUserInfo] = useState({});
+    const [userInfo, setUserInfo] = useState([]);
+
+    useEffect(() => {
+        let token = localStorage.getItem('token') ? localStorage.getItem('token') : false;
+
+    }, []);
 
   return (
       <Router>
+          <PlantContext.Provider value={{userInfo, setUserInfo}}>
           <div>
-              <UserContext.Provider value={userInfo}>
-                  <Navigation/>
-              </UserContext.Provider>
+              <Navigation/>
 
-              {localStorage.getItem('token') ? <PrivateRoute component={PlantPage}/> : <Route exact path="/" component={MarketingPage}/>}
+              {userInfo.id ? <PrivateRoute component={PlantList}/> : <Route exact path="/" component={MarketingPage}/>}
           </div>
+          </PlantContext.Provider>
       </Router>
   );
 }
--- src/components/plants/plantList.js
+++ src/components/plants/plantList.js
@@ -1,19 +1,40 @@
-import React, {useContext} from "react";
+import React, {useContext, useEffect} from "react";
 import PlantDetail from "./plantDetail";
 import PlantContext from "../../contexts/plantsContext";
+import {call_get, PLANTS} from "../../api/apiHelpers";
 
 const PlantList = () => {
-    const plants = useContext(PlantContext);
+    const {userInfo, setUserInfo} = useContext(PlantContext);
 
-    return(
-        <div>
-            {plants.map((plant) => {
-                return (
-                    <PlantDetail plant={plant}/>
-                );
-            })}
-        </div>
-    );
+    useEffect(() => {
+        call_get(PLANTS)
+            .then((response) => {
+                setUserInfo(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
+    console.log(userInfo)
+
+    if(userInfo.plants) {
+        return(
+            <div>
+                {userInfo.plants.map((plant) => {
+                    return (
+                        <PlantDetail plant={plant}/>
+                    );
+                })}
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                Loading ...
+            </div>
+        );
+    }
 }
 
 export default PlantList;
