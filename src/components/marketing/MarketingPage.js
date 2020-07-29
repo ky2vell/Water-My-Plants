e3a7e5eb58819b31db1f7dc0c34dd91eba17227e
Kyle Lovell <ky2vell@gmail.com>
2020-07-29 03:57:57 -0400

    mobile styling

--- src/components/marketing/MarketingPage.js
+++ src/components/marketing/MarketingPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const MarketingPage = () => {
-    return (
-      <div>
-          Marketing ...
-      </div>
-    );
+  return (
+    <div style={{ textAlign: 'center', fontSize: '4rem', paddingTop: '5rem' }}>
+      <i className='fas fa-tools' style={{ marginRight: '2rem' }}></i>
+      HomePage Under Construction ...
+    </div>
+  );
 };
 
 export default MarketingPage;
